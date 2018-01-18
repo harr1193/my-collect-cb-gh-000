5361c9da0bb01c10b58034634db3f23e7d04486d
Kyle Harris <k_harris95@live.com>
2018-01-18 03:02:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,13 +2,10 @@ def my_collect(collection)
   i = 0
   new_collection = []
   while i < collection.length
+    binding.pry
     new_collection << yield(collection)
     i += 1
   end
   new_collection
 end
 
-collection = ['ruby', 'javascript', 'python', 'objective-c']
-puts my_collect(collection) do |lang|
-  lang.upcase
-end
